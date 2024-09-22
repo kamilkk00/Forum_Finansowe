3ecdbd7db39c6a24a213af880f72c6bbd18a0e22
Kamil <CEM@macbook-air.home>
2024-09-22 22:40:41 +0200

    Poprawiono literówkę w nazwie django-markdown-deux

--- final_project/settings.py
+++ final_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['forum-finansowe.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'django_markdown_deux',
+    'django-markdown-deux',
     'finance',
     'django.contrib.admin',
     'django.contrib.auth',
